7685cdf69e90358ea46247cff47722814667462e
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-05-31 13:21:42 +0300

    Настроен webpack + файл разбит на модули

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
-const path = require(`path`);
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "public"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
-    contentBase: path.join(__dirname, "public"),
-    watchContentBase: true
-  },
-
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+  }
 };
