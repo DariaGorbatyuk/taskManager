116b24bd175e6bb43ae3a558121279688cf913b6
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-06-21 16:59:02 +0300

    Добавляет новые задачи по клику на кнопку

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import {createLoadBtn} from "./view/loadButton.js";
 import {generateFilter} from "./mock/generateFilter.js";
 import {generateTask} from "./mock/generateTask.js";
 
-const TASK_COUNT = 4;
+
+const TASK_COUNT = 22;
+const TASK_COUNT_ON_START = 8;
+const TASK_COUNT_SHOWING_ON_BTN = 8;
 const TASKS = new Array(TASK_COUNT).fill().map(generateTask);
 
 const mainHeader = document.querySelector(`.main__control`);
@@ -22,9 +25,23 @@ renderElement(main, createBoard(), `beforeend`);
 
 const board = document.querySelector(`.board`);
 const boardTasks = document.querySelector(`.board__tasks`);
-
+let showingTaskCount = TASK_COUNT_ON_START;
 renderElement(boardTasks, createTaskEditTemplate(TASKS[0]), `beforeend`);
-for (let i = 1; i < TASK_COUNT; i++) {
+for (let i = 1; i < TASK_COUNT_ON_START; i++) {
   renderElement(boardTasks, createTask(TASKS[i]), `beforeend`);
 }
 renderElement(board, createLoadBtn(), `beforeend`);
+const loadBtn = document.querySelector(`.load-more`);
+loadBtn.addEventListener(`click`, onLoadBtnClick);
+
+function onLoadBtnClick(evt) {
+  evt.preventDefault();
+  const prevTaskCount = showingTaskCount;
+  showingTaskCount = showingTaskCount + TASK_COUNT_SHOWING_ON_BTN;
+  TASKS.slice(prevTaskCount, showingTaskCount).forEach((task) => {
+    renderElement(boardTasks, createTask(task), `beforeend`);
+  });
+  if (showingTaskCount > TASK_COUNT) {
+    loadBtn.remove();
+  }
+}
